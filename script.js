9d4dc3b15637a642e7eb6fd32eb5ffc24f21c0b8
lemon <D1249595@o365.fcu.edu.tw>
2025-03-04 01:33:46 +0800

    新增標題跟導覽列

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("logo").addEventListener("click", function() {
+    window.location.href = "https://www.animate-onlineshop.com.tw/?srsltid=AfmBOopIvG-480MjW-U_Eq2fOB5jgc8k3XomJ_SiF0WBH-wf64CIGii8";
+});
